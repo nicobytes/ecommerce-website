78570f5a2992bd7e6eef4a1f39856c99589b4559
Nicolas Molina Monroy <hi@nicobytes.com>
2024-03-01 18:03:45 -0400

    Remove timeAgo pipe from product component HTML and import statement

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -2,12 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { Product } from '@models/product.model';
-import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, TimeAgoPipe, RouterLinkWithHref],
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './product.component.html',
 })
 export class ProductComponent {
